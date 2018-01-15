5dda7fc8b8da875c0d2b8e99476aac29ce838ffd
iflycn <iflycn@gmail.com>
2018-01-15 13:49:58 +0800

    20180115 - 芝士超人 APP 适配

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# 百万英雄答题器
-为西瓜视频的百万英雄活动开发的 `Python` 版答题器，支持 Android 手机或电脑模拟器。
+# 派桑答题助手
+使用 `Python` 开发的答题助手，目前适配百万英雄、芝士超人，支持 Android 手机或电脑模拟器。
 
 ## 运行效果截图
 ![](https://github.com/iflycn/hero/blob/master/cmd.png)
@@ -19,12 +19,12 @@
 3. 开启开发者选项，打开 USB 调试，小米手机一并打开 USB 调试（安全设置）
 4. 使用数据线连接手机，等待系统自动安装驱动完成
 ### 下载程序
-5. 下载[百万英雄答题器压缩包](https://github.com/iflycn/hero/archive/master.zip)，解压到硬盘目录，例如 `D:\hero-master`
+5. 下载[派桑答题助手压缩包](https://github.com/iflycn/hero/archive/master.zip)，解压到硬盘目录，例如 `D:\hero-master`
 6. 修改文件 `mian.py` 中的 `APP_ID`、`API_KEY`、`SECRET_KEY` 为你自己的，申请地址：[百度 AI 开放平台](http://ai.baidu.com/tech/ocr/general)
 ### 安装 Python
 7. 从 [Python 官网](https://www.python.org/downloads)下载安装 `python3.6.4`
 8. 电脑运行 `CMD`，输入命令 `pip3 install -r D:\hero-master\requirements.txt`，等待下载依赖包并安装完成
-9. 如果下载依赖包过程中出错，重新输入命令 `pip3 install -r requirements.txt` 直到下载成功
+9. 如果下载依赖包过程中出错，重新输入命令 `pip3 install -r D:\hero-master\requirements.txt` 直到下载成功
 ### 开始使用
 10. 手机进入百万英雄界面
 11. 电脑运行 `CMD`，依次输入命令 `D:`、`cd hero-master`、`python main.py`，根据程序提示操作即可
--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from aip import AipOcr
 from ai import AI
 
 # 你的百度 OCR
-APP_ID = "10676651"
-API_KEY = "66kRY7ZHxNY6z60Ot0cUD1Kf"
-SECRET_KEY = "ioQpGWnYABpDbBQ8edTkwnaMhG908MPH"
+APP_ID = "10675166"
+API_KEY = "66kRY7ZHxNY6z60Ot0cUKfD1"
+SECRET_KEY = "ioQpGWnYABpDbBQ8edTkwnaMhG90PH8M"
 
 # 处理设备截图
-def adb_get_screen(enter):
+def adb_get_screen(enter, app):
     # 获取设备截图
     os.system("adb shell /system/bin/screencap -p /sdcard/screenshot.png")
     os.system("adb pull /sdcard/screenshot.png ./screenshots/screenshot.png")
@@ -25,7 +25,11 @@ def adb_get_screen(enter):
     except:
         print("error: file not found")
         sys.exit()
-    region = img.crop((70, 240, img.size[0] - 70, 1285))  # 截图裁剪坐标
+    if app == "2": # 截图裁剪坐标
+        coordinate = (45, 285, 45, 1200) # 芝士超人
+    else:
+        coordinate = (70, 240, 70, 1285) # 百万英雄
+    region = img.crop((coordinate[0], coordinate[1], img.size[0] - coordinate[2], coordinate[3]))
     region.save(r"./screenshots/screenshot_crop.png")
 
 # 获取 OCR 数据
@@ -34,10 +38,10 @@ def get_crop_data(img):
         return fp.read()
     return ""
 
-def main(enter):
+def main(enter, app):
     time_start = time.time()
     # 处理设备截图
-    adb_get_screen(enter)
+    adb_get_screen(enter, app)
     # 获取 OCR 结果
     OCR = AipOcr(APP_ID, API_KEY, SECRET_KEY)
     try:
@@ -63,12 +67,13 @@ def main(enter):
     print("use {0} seconds".format(round(time_end - time_start, 2)))
 
 if __name__ == "__main__":
-    print("\n" + "-" * 28 + " 百万英雄答题器 " + "-" * 28)
+    print("\n" + "-" * 29 + " 派桑答题助手 " + "-" * 29)
     print("\n答案抓取自问答网站，无法保证绝对正确，如果回答和你所知不符，请相信自己！")
+    app = input("\n输入数字代表的 APP 类型（1. 百万英雄、2. 芝士超人）：")
     print("\n屏幕出现完整题目后按回车键，如果运行中出错，按 CTRL+C 退出并重新运行。")
     while True:
         enter = input("\n回车键作答，q 键退出。等待输入：")
         if enter.lower() == "q":
             break
         else:
-            main(enter)
+            main(enter, app)
