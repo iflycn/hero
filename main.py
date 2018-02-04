05b155f24eb13b074ade46a046fc62ce992d93d2
iflycn <iflycn@gmail.com>
2018-02-04 18:12:03 +0800

    20180204 - 修正黄金十秒坐标

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ def get_config_data(section, key):
         return config.get(section, key)
     except:
         print("error: read config.ini error")
-        time.sleep(3)
-        sys.exit()
+        return ""
 
 # 处理设备截图
 def adb_get_screen(enter, app):
@@ -42,12 +41,12 @@ def adb_get_screen(enter, app):
             X2 = int(get_config_data("COORDINATE", "X2"))
             Y2 = int(get_config_data("COORDINATE", "Y2"))
         except:
-            print("error: COORDINATE must be INI")
+            print("error: COORDINATE must be INT")
             time.sleep(3)
             sys.exit()
         region = img.crop((X1, Y1, X2, Y2))
     else:
-        coordinate = ((70, 240, 70, 1285), (45, 285, 45, 1150), (70, 310, 70, 1160), (60, 300, 60, 1160), (100, 320, 100, 1160), (40, 375, 40, 1320), (70, 260, 70, 1100), (100, 540, 100, 1280), (80, 1100, 80, 1740), (90, 440, 90, 1220), (60, 310, 60, 1180), (80, 420, 80, 1110), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (138, 500, 138, 1700))
+        coordinate = ((70, 240, 70, 1285), (45, 285, 45, 1150), (70, 310, 70, 1160), (60, 300, 60, 1160), (90, 320, 90, 1160), (40, 375, 40, 1320), (70, 260, 70, 1100), (100, 540, 100, 1280), (80, 1100, 80, 1740), (90, 440, 90, 1220), (60, 310, 60, 1180), (80, 420, 80, 1110), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (0, 0, 0, 1920), (138, 500, 138, 1700))
         coordinate = coordinate[app - 1]
         coefficient = img.size[0] / 1080
         region = img.crop((coordinate[0] * coefficient, coordinate[1] * coefficient, img.size[0] - coordinate[2] * coefficient, coordinate[3] * coefficient))
@@ -104,7 +103,7 @@ if __name__ == "__main__":
     ctypes.windll.kernel32.SetConsoleTextAttribute(ctypes.windll.kernel32.GetStdHandle(-11), 0x07)
     print("-" * 72)
     print("{}百万英雄答题助手".format(" " * 28))
-    print("{}1.5.5.20180202".format(" " * 29))
+    print("{}1.5.6.20180202".format(" " * 29))
     print("-" * 72)
     print("\n答案抓取自问答网站，无法保证绝对正确，如果回答和你所知不符，请相信自己！\n")
     print("A. 百万英雄\tB. 芝士超人\tC. 冲顶大会\nD. 百万赢家\tE. 黄金十秒\tF. 全民答题\nG. 非答不可\tH. 蘑菇大富翁\tI. 百万黄金屋\nJ. 极速挑战\tK. 小米有乐\tL. 百万文豪\nX. 头脑王者\tZ. 自定义\n")
